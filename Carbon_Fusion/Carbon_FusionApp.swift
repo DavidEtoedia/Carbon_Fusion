eac6863126427156ee48e66fc4a111facb9d393c
DavidEtoedia <etoedia@gmail.com>
2023-05-16 15:23:49 -0700

    Initial Commit

---
+++ Carbon_Fusion/Carbon_FusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carbon_FusionApp.swift
+//  Carbon_Fusion
+//
+//  Created by Inyene Etoedia on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Carbon_FusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
